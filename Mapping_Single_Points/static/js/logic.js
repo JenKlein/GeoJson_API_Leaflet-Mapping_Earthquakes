8c5d2a4f0668e310cf399be4281f41a952378044
Jennifer Klein <jenniferklein@Jennifers-MacBook-Air.local>
2021-05-22 13:14:47 -0400

    mapping multiple markers

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -14,6 +14,10 @@ console.log("working");
 // // Then we add our 'graymap' tile layer to the map.
 // streets.addTo(map);
 
+
+
+
+
 // with mapbox
 
 //  Add a marker to the map for Los Angeles, California.
@@ -22,19 +26,47 @@ console.log("working");
 // Create the map object with a center and zoom level.
 let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 
-//  Add a circle marker to the map for Los Angeles, California.
-L.circleMarker([34.0522, -118.2437], {
-    color: 'black',
-    radius: 300,
-    fillColor: '#ffffa1'
- }).addTo(map);
+// //  Add a circle marker to the map for Los Angeles, California.
+// L.circleMarker([34.0522, -118.2437], {
+//     color: 'black',
+//     radius: 300,
+//     fillColor: '#ffffa1'
+//  }).addTo(map);
  
+
+// Get data from cities.js
+let cityData = cities;
+
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+    console.log(city)
+    L.circleMarker(city.location, {
+        radius: city.population /100000
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population + "</h3>")
+    .addTo(map);
+});
+
+
+// Skill drill 13.4.2
+
+cityData.forEach(function(city) {
+    console.log(city)
+    L.circleMarker(city.location, {
+        radius: city.population -200000, 
+        color: 'orange',
+        fillColor: '#f03'
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population + "</h3>")
+    .addTo(map);
+});
+
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    accessToken: API_KEY
+    accessToken: API_KEY // how does it know to reference this in the separate config file? 
 });
 
 // Then we add our 'graymap' tile layer to the map.
