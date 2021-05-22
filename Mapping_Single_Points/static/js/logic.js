0c28d08de518be902e9eb1790f758b817607c6aa
Jennifer Klein <jenniferklein@Jennifers-MacBook-Air.local>
2021-05-22 14:38:58 -0400

    corrected logic.js file + API key + cities.js

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -37,6 +37,8 @@ let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 // Get data from cities.js
 let cityData = cities;
 
+
+
 // Loop through the cities array and create one marker for each city.
 cityData.forEach(function(city) {
     console.log(city)
@@ -50,20 +52,20 @@ cityData.forEach(function(city) {
 
 // Skill drill 13.4.2
 
-cityData.forEach(function(city) {
-    console.log(city)
-    L.circleMarker(city.location, {
-        radius: city.population -200000, 
-        color: 'orange',
-        fillColor: '#f03'
-    })
-    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population + "</h3>")
-    .addTo(map);
-});
+// cityData.forEach(function(city) {
+//     console.log(city)
+//     L.circleMarker(city.location, {
+//         radius: city.population -200000, 
+//         color: 'orange',
+//         fillColor: '#f03'
+//     })
+//     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population + "</h3>")
+//     .addTo(map);
+// });
 
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY // how does it know to reference this in the separate config file? 
